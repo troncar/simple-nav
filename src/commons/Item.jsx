f6e4b3fc1737d7ca9611c39fe71b7524afc1ee7e
Nuria Tronco <nuria.tronco@gmail.com>
2020-02-13 20:24:18 -0600

    Add styles and event for items

--- src/commons/Item.jsx
+++ src/commons/Item.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Item.scss';
 
 const Item = (props) => {
     return (
-    <div className={`item--${props.type}`}>{props.label}</div>);
+    <div className={`item item--${props.type} item--${props.position}`} action={props.action} name={props.name} url={props.url} onClick={props.handler}>{props.label}</div>);
 }
  
 export default Item;
--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -5,15 +5,39 @@ import Logo from '../commons/Logo.jsx';
 import "./NavBar.scss";
 
 class NavBar extends Component {
+
     state = {  }
+
+    handlerClick = (event) => {
+        console.log(event.target.getAttribute('name'));
+        console.log(event.target.getAttribute('action'));
+    }
+
     render() { 
         return (  
             <div className="navbar">
                 <div className="navbar__container">
                     <Logo></Logo>
-                    <Item type={'first'}
-                        label={'About us'}
-                    ></Item>
+                    <div className="item__container">
+                        <Item   type={'first'}
+                                label={'About us'}
+                                position={'right'}
+                                handler={this.handlerClick}
+                                name={'about-us'}
+                                action={'redirect'}
+                                url={'https://www.google.com.mx/'}
+                        ></Item>
+                        <Item   type={'first'}
+                                label={'Login'}
+                                position={'right'}
+                                handler={this.handlerClick}
+                        ></Item>
+                        <Item   type={'secondary'}
+                                label={'Contact us'}
+                                position={'right'}
+                                handler={this.handlerClick}
+                        ></Item>
+                    </div>
                 </div>
             </div>
         );
