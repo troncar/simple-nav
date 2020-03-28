cbe71a7a8df0506c8517ca6bced77b353e4a90fb
Nuria Tronco <nuria.tronco@gmail.com>
2020-03-28 17:02:46 -0600

    Add new styles mobile item

--- src/commons/Item.jsx
+++ src/commons/Item.jsx
@@ -3,7 +3,7 @@ import './Item.scss';
 
 const Item = (props) => {
     return (
-    <div className={`item item--${props.type} item--${props.position}`} action={props.action} name={props.name} url={props.url} onClick={props.handler}>{props.label}</div>);
+    <div className={`item item--${props.type} item--${props.position} ${props.customClass ? props.customClass : ''}`} action={props.action} name={props.name} url={props.url} onClick={props.handler}>{props.label}</div>);
 }
- 
+
 export default Item;
--- src/commons/MenuMobile.jsx
+++ src/commons/MenuMobile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./MenuMobile.scss";
-// import Item  from './Item.jsx';
+import Item  from './Item.jsx';
 
 class MenuMobile extends Component {
   state = {  }
@@ -8,7 +8,36 @@ class MenuMobile extends Component {
     return (
 
     <div className= {`menu__mobile ${this.props.display ? 'show' : 'hidden'}`}>
-
+       <Item
+                  type={'first'} l
+                  label={'About us'}
+                  position={'right'}
+                  handler={this.handlerClick}
+                  name={'about-us'}
+                  action={'redirect'}
+                  customClass = {'item--mobile'}
+                  url={'https://www.youtube.com.mx/'}
+                />
+                <Item
+                  type={'first'}
+                  label={'Login'}
+                  position={'right'}
+                  name={'about-us'}
+                  action={'redirect'}
+                  customClass = {'item--mobile'}
+                  url={'https://www.facebook.com.mx/'}
+                  handler={this.handlerClick}
+                />
+                <Item
+                  type={'secondary'}
+                  label={'Contact us'}
+                  position={'right'}
+                  name={'about-us'}
+                  action={'redirect'}
+                  customClass = {'item--mobile'}
+                  url={'https://www.google.com.mx/'}
+                  handler={this.handlerClick}
+                />
     </div> );
   }
 }
