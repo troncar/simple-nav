460366d4a89c77b44e9cbe9e341b11f25dc02f0d
Nuria Tronco <nuria.tronco@gmail.com>
2020-04-26 20:49:10 -0500

    Updates

---
+++ precache-manifest.669465db852c395e00b362bec75946d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50689c3f20d8c02dd9f185496cbb7a85",
+    "url": "/simple-nav/index.html"
+  },
+  {
+    "revision": "0e812369466b8012c954",
+    "url": "/simple-nav/static/css/main.c6c26a78.chunk.css"
+  },
+  {
+    "revision": "3f48491de99bfb52bf00",
+    "url": "/simple-nav/static/js/2.6bf9fb89.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-nav/static/js/2.6bf9fb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e812369466b8012c954",
+    "url": "/simple-nav/static/js/main.a956658c.chunk.js"
+  },
+  {
+    "revision": "342c644d9c81ccae2cd9",
+    "url": "/simple-nav/static/js/runtime-main.043d0e44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-nav/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/commons/Item.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Item.scss';
-
-const Item = (props) => {
-    return (
-    <div className={`item item--${props.type} item--${props.position} ${props.customClass ? props.customClass : ''}`} action={props.action} name={props.name} url={props.url} onClick={props.handler}>{props.label}</div>);
-}
-
-export default Item;
